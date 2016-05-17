d4fc95229dae3d15c206fd11d2523768ee2641d8
Ronnie Mayberry <ronnielmayberry@gmail.com>
2016-05-17 16:09:25 -0500

    ss

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class SinglyLinkedList {
 	private ListNode first, last;
@@ -17,17 +20,17 @@ public class SinglyLinkedList {
 			return first.getValue();
 	}
 
+	public void addFirst(Object value) {
+		first = new ListNode(value, first);
+		numNodes++;
+	}
+
 	public Object getLast() {
 		if (last == null) {
 			throw new NoSuchElementException();
 		} else {
 			return last.getValue();
 		}
-	}
-
-	public void addFirst(Object value) {
-		first = new ListNode(value, first);
-		numNodes++;
 	}
 
 	public void addLast(Object value) {
@@ -42,22 +45,42 @@ public class SinglyLinkedList {
 		numNodes++;
 	}
 
-	public int size() {
-		return numNodes;
-	}
-
-	public void insert(Object value) {
-
+	public void insert(Item value) {
+		boolean found = false;
+		if (first == null) {
+			addFirst(value);
+			last = first;
+			return;
+		}
+		ListNode prev = first;
+		ListNode curr = first;
+		ListNode n = new ListNode(value, null);
+		
+		while((curr != null) && (value.compareTo(curr.getValue()) > 0)) {
+			prev = curr;
+			curr = curr.getNext();
+		} if (curr == first) {
+			n.setNext(curr);
+			first = n;
+		} else if (prev == last) {
+			prev.setNext(n);
+			last = n;
+		} else {
+			n.setNext(curr);
+			prev.setNext(n);
+		}
 	}
 
 	public ListNode find(Object value) {
-		ListNode ref = null;
-
-		return ref;
+		return null;
 	}
 
 	public void remove() {
+// https://github.com/kongd/APCS/blob/master/SinglyLinkedList.java
+	}
 
+	public int size() {
+		return numNodes;
 	}
 
 	public void clear() {
@@ -65,7 +88,23 @@ public class SinglyLinkedList {
 	}
 
 	public void loadData() {
+		clear();
+		File afile = new File("file20.txt");
+		Scanner fromfile = null;
+		int id, inv;
 
+		try {
+			fromfile = new Scanner(afile);
+		} catch (FileNotFoundException e) {
+			System.out.println("\nFile not found!\n");
+			System.exit(1);
+		}
+		while (fromfile.hasNext()) {
+			id = fromfile.nextInt();
+			inv = fromfile.nextInt();
+			Item i = new Item(id, inv);
+			insert(i);
+		}
 	}
 
 	public void printBackward() {
